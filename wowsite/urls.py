90765db5c1fac478f29dc1f36e15c7e9815873ad
zerogvt <gkoumplias@gmail.com>
2017-03-09 15:18:34 +0000

    --

--- wowsite/urls.py
+++ wowsite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin$', admin.site.urls),
-    url(r'^fortunebot$', include('fortunebot.urls')),
-    url(r'^webhook$', include('fortunebot.urls')),
+    url(r'^admin', admin.site.urls),
+    url(r'^fortunebot', include('fortunebot.urls')),
+    url(r'^webhook', include('fortunebot.urls')),
 ]
