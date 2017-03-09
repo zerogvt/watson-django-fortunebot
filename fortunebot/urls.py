c2920431cb124cbb2ea1a0a5fdd391e1bdc56423
zerogvt <gkoumplias@gmail.com>
2017-03-09 15:23:21 +0000

    --

--- fortunebot/urls.py
+++ fortunebot/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^webhook$', views.webhook, name='webhook'),
+    url(r'^webhook[/]*$', views.webhook, name='webhook'),
 ]
