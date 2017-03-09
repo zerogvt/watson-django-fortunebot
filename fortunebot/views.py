fe343b04a3060ca5ae1bcd520992a71e8a71a79e
zerogvt <gkoumplias@gmail.com>
2017-03-09 16:40:51 +0000

    --

--- fortunebot/views.py
+++ fortunebot/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from . import webhooks
 
 def index(request):
-    return HttpResponse("INDEX")
+    return HttpResponse("IIINDEX")
 
 def webhook(request):
     return webhooks.handle(request)
