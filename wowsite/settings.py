98c5454ed9d5b94d63b8ceae1d2b8ff928bf54b6
zerogvt <gkoumplias@gmail.com>
2017-03-09 14:30:20 +0000

    init

--- wowsite/settings.py
+++ wowsite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
