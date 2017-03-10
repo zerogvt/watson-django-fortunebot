81cd28e24d5b30803db8ac1a32c497e706a7de12
zerogvt <gkoumplias@gmail.com>
2017-03-10 14:02:03 +0000

    --

--- fortunebot/views.py
+++ fortunebot/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.views.decorators.csrf import requires_csrf_token
+from django.views.decorators.csrf import ensure_csrf_cookie
 # Create your views here.
 
 from . import webhooks
@@ -7,6 +7,6 @@ from . import webhooks
 def index(request):
     return HttpResponse("INDEX")
 
-@requires_csrf_token
+@ensure_csrf_cookie
 def webhook(request):
     return webhooks.handle(request)
