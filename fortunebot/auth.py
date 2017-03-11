059ab306f1746eb1d01c41ad5f43f37da585a4d8
zerogvt <gkoumplias@gmail.com>
2017-03-11 20:35:08 +0000

    --

--- fortunebot/auth.py
+++ fortunebot/auth.py
@@ -19,7 +19,7 @@ def authenticateApp():
     payload = {"grant_type": "client_credentials"}
     headers = {"Authorization": "Basic %s" % base64.b64encode(credentials)}
     try:
-        print(base64.decodestring("%s", base64.b64encode(credentials)))
+        print(base64.decodestring("%s" % base64.b64encode(credentials)))
     except binascii.Error:
         print("no correct base64")
     # POST to /oauth/token to obtain access token
