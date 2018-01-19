8a3ab94a5eab0f2712dec88b9ffa37ed55df5b83
vasigkou <vasigkou@ie.ibm.com>
2018-01-19 15:43:57 +0200

    better naming

--- wowsite/settings.py
+++ wowsite/settings.py
@@ -23,13 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if 'PRODUCTION' in os.environ:
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['watson-on-work.herokuapp.com',
                  'wow-delivery.mybluemix.net',
